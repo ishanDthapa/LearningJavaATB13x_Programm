fd074511e64bf036edcc479a341857b315942d7b
ishanDthapa <ishantthapa895@gmail.com>
2025-06-28 19:06:06 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/lab001_HelloWord.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class lab001_HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/ex_01_Java_Basic/lab002_Addition.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class lab002_Addition {
+    public static void main(String[] args) {
+        System.out.println(20+20);
+    }
+}
